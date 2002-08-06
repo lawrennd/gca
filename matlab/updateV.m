c7f63785691b8d0c0ab9d49fe5295f7fcfcefe9f
Neil Lawrence <lawrennd@gmail.com>
2002-08-06 11:07:50 +0000

    Made some changes

--- matlab/updateV.m
+++ matlab/updateV.m
@@ -1,13 +1,18 @@
-function [V, d] = updateV(sbar, Sigma_s, XUL)
+function [V, d] = updateV(sbar, Sigma_s, XUL, choose)
+
 
 dataDim = size(XUL, 2);
 latentDim = size(sbar, 2);
 ndata = size(XUL, 1);
+
+if nargin < 4
+  choose = 1:ndata;
+end
 d = ones(1, latentDim);
 
 
 sum_ssT = zeros(latentDim);
-for n = 1:ndata
+for n = choose
   sum_ssT = sum_ssT + sbar(n, :)'*sbar(n, :) + Sigma_s(:, :, n);
 end
 C = chol(sum_ssT);
@@ -16,5 +21,5 @@ inv_sum_ssT = Cinv*Cinv';
 
 
 for i = 1:dataDim
-  V(:, i) = (XUL(:, i)'*sbar*inv_sum_ssT)';
+  V(:, i) = (XUL(choose, i)'*sbar(choose, :)*inv_sum_ssT)';
 end
--- matlab/updatelatent.m
+++ matlab/updatelatent.m
@@ -1,13 +1,16 @@
-function [sbar, Sigma_s] = updatelatent(XUL, V, tau, beta, lambda) 
+function [sbar, Sigma_s] = updatelatent(XUL, V, tau, beta, lambda, choose) 
 
 ndata = size(XUL, 1);
+if nargin < 6
+  choose = 1:ndata;
+end
 latentDim = size(tau, 2);
 dataDim = size(V, 1);
 sbar = zeros(ndata, latentDim);
 Sigma_s = zeros(latentDim, latentDim, ndata);
 B = diag(beta*lambda.*lambda);
 VBVT = V*B*V';
-for n = 1:ndata
+for n = choose
   invSigma_s = diag(tau(n, :)) + VBVT;
   C = chol(invSigma_s);
   Cinv = eye(latentDim)/C;
